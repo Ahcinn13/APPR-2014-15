1084519b159b14f0991083141b21df69fcb93509
Neza Ahcin <neza.ahcin@gmail.com>
2015-01-10 22:09:03 +0100

    zemljevidi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -8,6 +8,7 @@ source("lib/uvozi.zemljevid.r")
 
 
 cat("Uvazam zemljevid... \n")
+pdf("slike/zemljevid.pdf")
 svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip", 
                         "zemljevid.sveta", "ne_110m_admin_0_countries.shp", mapa = "zemljevid",
                         encoding = "Windows-1250")
@@ -26,38 +27,53 @@ m <- match(svet$admin, levels(factor(OI$Country)))
 svet$primeri <- c(0)
 svet$primeri[which(!is.na(m))]<- data.frame(table(OI$Country))$Freq[m[which(!is.na(m))]]
 nov.svet <- svet[svet$primeri !=0,]
-#svet$enake <- factor(svet$primeri)
-#spplot(svet, "primeri", col.regions = rainbow(12))
-#barve <- bpy.colors(length(levels(svet$enake)))[svet$enake]
-#spplot(svet, "primeri", col.regions = barve)
-#spplot(svet, "primeri", col.regions = c("white", rainbow(13)))
 
 
 
-
-
-#imena in koordinate 3 drzav(ZDA, RUS, BUL) z največ dopinskimi primeri na OI
+#imena in koordinate 2 drzav(ZDA, RUS) z največ dopinskimi primeri na OI
 
 RUS <- which(svet$admin == "Russia")
-BUL <- which(svet$admin == "Bulgaria")
 ZDA <- which(svet$admin == "United States of America")
-vektor <- c(RUS, BUL, ZDA)
+vektor <- c(RUS, ZDA)
 koordinate <- coordinates(svet[vektor,])
 rownames(koordinate) <- svet[vektor,]$admin
 
 
-spplot(svet, "primeri", col.regions = c("white", rainbow(16, start=0, end=11/13)), 
+print(spplot(svet, "primeri", col.regions = c("white", rainbow(16, start=0, end=11/13)), 
        main = "Stevilo dopinskih primerov po drzavah", 
-       sp.layout = list("sp.text", koordinate, svet[vektor,]$admin, cex=0.6),
-       par.settings = list(panel.background=list(col="lightblue")))
+       sp.layout = list("sp.text", koordinate, svet[vektor,]$admin, cex=0.8),
+       par.settings = list(panel.background=list(col="lightblue"))))
 
-nov.svet$substanca <- c("")
-#drzave s samo enim dopinškim primerom
-q <- nov.svet$admin[nov.svet$primeri ==1]
-q <- as.character(q)
-dr1 <- OI$Country[OI$Country %in% q]
-OI$Banned.substance[OI$Country %in% q]
-nov.svet$substance[match(dr1, nov.svet$admin)]<-OI$Banned.substance[OI$Country %in% q]
+
+svet$substanca <- c("")
+#naredimo novo tabelo upostevamo zgolj relavantne podatke
+mamila.poland <- data.frame(table(OI$Banned.substance[OI$Country =="Poland"]))
+svet$substanca[svet$admin == "Poland"] <- levels(factor(mamila.poland$Var1[mamila.poland$Freq == max(mamila.poland$Freq)]))
+
+mamila.hungary <- data.frame(table(OI$Banned.substance[OI$Country =="Hungary"]))
+svet$substanca[svet$admin == "Hungary"] <- levels(factor(mamila.hungary$Var1[mamila.hungary$Freq == max(mamila.hungary$Freq)]))
+
+mamila.greece <- data.frame(table(OI$Banned.substance[OI$Country =="Greece"]))
+svet$substanca[svet$admin == "Greece"] <- levels(factor(mamila.greece$Var1[mamila.greece$Freq == max(mamila.greece$Freq)]))
+
+mamila.bulgaria <- data.frame(table(OI$Banned.substance[OI$Country =="Bulgaria"]))
+svet$substanca[svet$admin == "Bulgaria"] <- levels(factor(mamila.bulgaria$Var1[mamila.bulgaria$Freq == max(mamila.bulgaria$Freq)]))
+
+mamila.usa <- data.frame(table(OI$Banned.substance[OI$Country =="United States of America"]))
+svet$substanca[svet$admin =="United States of America"] <- levels(factor(mamila.usa$Var1[mamila.usa$Freq == max(mamila.usa$Freq)]))
+
+mamila.russia <- data.frame(table(OI$Banned.substance[OI$Country =="Russia"]))
+svet$substanca[svet$admin == "Russia"] <- levels(factor(mamila.russia$Var1[mamila.russia$Freq == max(mamila.russia$Freq)]))
+
+svet$enake <- factor(svet$substanca)
+
+barve <- bpy.colors(length(levels(svet$enake)))[svet$enake]
+
+barve[barve == "#000033FF"]<-"white"
+plot(svet, col = barve, bg ="lightblue")
+legend("bottomleft", legend = levels(factor(svet$substanca[svet$substanca!= ""])),
+      fill = levels(factor(barve[svet$substanca != ""])), bg = "white")
+title("Najpogosteje uporabljeno nedovoljeno sredstvo")
 
 #tabela odvzetih medalj:
 medals <- merge(gold.medals, silver.medals, all = TRUE)
@@ -81,10 +97,10 @@ svet$odvzete.medalje[which(!is.na(ujemanje))]<- data.frame(table(medals$Country)
 
 #vekotr izpisanih drzave je isti kot v zgornjem primeru 
 
-spplot(svet, "odvzete.medalje", col.regions = c("white", rainbow(15, start=0, end = 10/12)),
+print(spplot(svet, "odvzete.medalje", col.regions = c("white", rainbow(15, start=0, end = 10/12)),
        main = "Stevilo odvzetih olimpijskih medalj zaradi dopinga",
-       sp.layout = list("sp.text", koordinate, svet[vektor,]$admin, cex=0.6),
-       par.settings = list(panel.background=list(col="lightblue")))
+       sp.layout = list("sp.text", koordinate, svet[vektor,]$admin, cex=0.8),
+       par.settings = list(panel.background=list(col="lightblue"))))
        
 
 OI$host.country[OI$Place == "Munich"] <- "Germany"
@@ -108,69 +124,27 @@ OI$host.country[OI$Place == "Barcelona"] <- "Spain"
 OI$host.country[OI$Place == "Calgary"] <- "Canada"
 
 
+frekvence <- data.frame(table(OI$host.country))
+
+frekvence$Freq[frekvence$Var1 == "United States of America"] <- frekvence$Freq[frekvence$Var1 == "United States of America"]/3
+frekvence$Freq[frekvence$Var1 == "Canada"] <- frekvence$Freq[frekvence$Var1 == "Canada"]/3
+
+
+uj <- match(svet$admin, frekvence$Var1)
+svet$frekvence <- c(0)
+svet$frekvence[which(!is.na(uj))] <- frekvence$Freq[uj[which(!is.na(uj))]]
+svet$frek1 <- factor(svet$frekvence)
+barve1 <- topo.colors(length(levels(svet$frek1)))[svet$frek1]
+barve1[barve1 == "#4C00FFFF"]<-"white"
+plot(svet, col=barve1, bg = "lightblue")
+title("Povprecno stevilo dopinskih primerov na OI")
+tabelabarv <- data.frame(svet$frekvence[svet$frekvence!= 0], barve1[svet$frekvence !=0])
+colnames(tabelabarv)<-c("stevilo", "barva")
+qw <- match(levels(factor(svet$frekvence[svet$frekvence!= 0])), tabelabarv$stevilo)
+tabelabarv <- tabelabarv[qw,]
+legend("bottomleft", legend = levels(factor(svet$frekvence[svet$frekvence!= 0])),
+       fill = c(as.character(tabelabarv$barva)), bg = "white")
 
 
 
-
-
-# Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
-preuredi <- function(podatki, zemljevid) {
-  nov.svet <- c()
-  manjkajo <- ! nov.svet %in% rownames(podatki)
-  M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-  names(M) <- names(podatki)
-  row.names(M) <- nov.svet[manjkajo]
-  podatki <- rbind(podatki, M)
-  
-  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$admin)[rank(zemljevid$admin)]), ]
-  if (ncol(podatki) == 1) {
-    out <- data.frame(out)
-    names(out) <- names(podatki)
-    rownames(out) <- rownames(podatki)
-  }
-  return(out)
-}
-
-
- 
-# # Uvozimo zemljevid.
-# cat("Uvažam zemljevid...\n")
-# obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-#                           "obcine", "OB/OB.shp", mapa = "zemljevid",
-#                           encoding = "Windows-1250")
-# 
-# # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
-# preuredi <- function(podatki, zemljevid) {
-#   nove.obcine <- c()
-#   manjkajo <- ! nove.obcine %in% rownames(podatki)
-#   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-#   names(M) <- names(podatki)
-#   row.names(M) <- nove.obcine[manjkajo]
-#   podatki <- rbind(podatki, M)
-#   
-#   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
-#   if (ncol(podatki) == 1) {
-#     out <- data.frame(out)
-#     names(out) <- names(podatki)
-#     rownames(out) <- rownames(podatki)
-#   }
-#   return(out)
-# }
-# 
-# # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-# druzine <- preuredi(druzine, obcine)
-# 
-# # Izračunamo povprečno velikost družine.
-# druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-# min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-# max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
-# 
-# # Narišimo zemljevid v PDF.
-# cat("Rišem zemljevid...\n")
-# pdf("slike/povprecna_druzina.pdf", width=6, height=4)
-# 
-# n = 100
-# barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
-# plot(obcine, col = barve)
-# 
-# dev.off()
+dev.off()
